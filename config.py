647ecc23b5f70ede22202ad8c120f7c0be418c57
krishnarjun00 <156284133+krishnarjun00@users.noreply.github.com>
2024-06-08 16:40:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 20299588
 api_hash = "f550d6179131c293d658f15f8c24f594"
 bot_token = "6585855832:AAE-gQB4MwC-J0nhUi3LjSo_2dCZTc0vwzQ"
-auth_users = "5374602611"
+auth_users = "5829511291"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "5374602611"
+osowner_users = "5829511291"
 owner_users = [int(num) for num in osowner_users.split(",")]
